32c48284c39879cf99114eb00854760a1e53b737
Aimila Maimaitijiang <almira1612@github.com>
2022-09-29 23:06:26 -0400

    Added Controller file

--- src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
+++ src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
@@ -1,2 +1,26 @@
-package com.cydeo.employeeregistrationproject.model;public class Employee {
+package com.cydeo.employeeregistrationproject.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
 }
