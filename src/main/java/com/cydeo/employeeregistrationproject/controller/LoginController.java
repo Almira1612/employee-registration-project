3dc56695df866aa82c99ce17a3d8b15e8ed00c9e
Aimila Maimaitijiang <almira1612@github.com>
2022-10-04 23:59:31 -0400

    Edit on controller file

--- src/main/java/com/cydeo/employeeregistrationproject/controller/LoginController.java
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.cydeo.employeeregistrationproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class LoginController {
 
     @RequestMapping(value = {"/login", "/"})
